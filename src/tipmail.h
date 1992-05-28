5ed092adf5c68f1f13492b7b26098bd0fda26d33
deyke <deyke>
1992-05-28 13:50:57 +0000

    Initial revision

---
+++ src/tipmail.h
@@ -0,0 +1,6 @@
+/* @(#) $Header: /home/deyke/tmp/cvs/tcp/src/tipmail.h,v 1.1 1992-05-28 13:50:57 deyke Exp $ */
+
+int tipstart __ARGS((int argc,char *argv[],void *p));
+int tip0 __ARGS((int argc,char *argv[],void *p));
+int telnet0 __ARGS((int argc,char *argv[],void *p));
+int telnet1 __ARGS((int argc,char *argv[],void *p));
