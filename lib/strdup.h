b4cc6fd6e5884fdba6758f767422a06d4fe85e4e
deyke <deyke>
1993-06-10 09:48:12 +0000

    Initial revision

---
+++ lib/strdup.h
@@ -0,0 +1,9 @@
+/* @(#) $Header: /home/deyke/tmp/cvs/tcp/lib/strdup.h,v 1.1 1993-06-10 09:48:12 deyke Exp $ */
+
+#ifndef _STRDUP_H
+#define _STRDUP_H
+
+/* In strdup.c: */
+char *strdup();
+
+#endif  /* _STRDUP_H */
