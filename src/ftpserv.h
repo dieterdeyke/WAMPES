c280d88517bdd7a37f6264b90cbfe063cb3384ae
deyke <deyke>
1993-12-29 16:32:42 +0000

    Initial revision

---
+++ src/ftpserv.h
@@ -0,0 +1,40 @@
+/* @(#) $Header: /home/deyke/tmp/cvs/tcp/src/ftpserv.h,v 1.1 1993-12-29 16:32:42 deyke Exp $ */
+
+#ifndef _FTPSERV_H
+#define _FTPSERV_H
+
+/* FTP commands */
+#define USER_CMD        0
+#define ACCT_CMD        1
+#define PASS_CMD        2
+#define TYPE_CMD        3
+#define LIST_CMD        4
+#define CWD_CMD         5
+#define DELE_CMD        6
+#define NAME_CMD        7
+#define QUIT_CMD        8
+#define RETR_CMD        9
+#define STOR_CMD        10
+#define PORT_CMD        11
+#define NLST_CMD        12
+#define PWD_CMD         13
+#define XPWD_CMD        14
+#define MKD_CMD         15
+#define XMKD_CMD        16
+#define XRMD_CMD        17
+#define RMD_CMD         18
+#define STRU_CMD        19
+#define MODE_CMD        20
+#define SYST_CMD        21
+#define XMD5_CMD        22
+#define APPE_CMD        23
+#define CDUP_CMD        24
+#define HELP_CMD        25
+#define MDTM_CMD        26
+#define NOOP_CMD        27
+#define REST_CMD        28
+#define SIZE_CMD        29
+#define XCUP_CMD        30
+#define XCWD_CMD        31
+
+#endif  /* _FTPSERV_H */
