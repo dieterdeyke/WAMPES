bac9d64c0b8ddc8543617b5ec1ed14d91964125f
deyke <deyke>
1994-01-18 18:50:30 +0000

    *** empty log message ***

--- src/version.c
+++ src/version.c
@@ -1,8 +1,8 @@
-/* @(#) $Header: /home/deyke/tmp/cvs/tcp/src/version.c,v 1.162 1994-01-17 14:53:06 deyke Exp $ */
+/* @(#) $Header: /home/deyke/tmp/cvs/tcp/src/version.c,v 1.163 1994-01-18 18:50:46 deyke Exp $ */
 
 #ifndef __lint
-static char id[] = "@(#)WAMPES-940117";
+static char id[] = "@(#)WAMPES-940118";
 #endif
 
 /* version control information */
-char Version[] = "WAMPES-940117";
+char Version[] = "WAMPES-940118";
