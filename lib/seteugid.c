fc9bc97fd4be9dc0286766ebbb03f5744e0aa02a
deyke <deyke>
1994-04-11 11:07:49 +0000

    *** empty log message ***

--- lib/seteugid.c
+++ lib/seteugid.c
@@ -1,4 +1,4 @@
-/* @(#) $Header: /home/deyke/tmp/cvs/tcp/lib/seteugid.c,v 1.2 1994-04-09 15:30:51 deyke Exp $ */
+/* @(#) $Header: /home/deyke/tmp/cvs/tcp/lib/seteugid.c,v 1.3 1994-04-11 11:09:58 deyke Exp $ */
 
 #include <sys/types.h>
 #include <unistd.h>
@@ -12,15 +12,15 @@
 void seteugid(int uid, int gid)
 {
   if (uid) {
+    setuid(0);
     setgid(gid);
 #if defined __hpux
-    setresuid(uid, uid, -1);
+    setresuid(uid, uid, 0);
 #elif defined sun
     seteuid(uid);
 #elif defined _AIX
     setuidx(ID_REAL | ID_EFFECTIVE, uid);
 #else
-    setreuid(0, 0);
     setreuid(0, uid);
 #endif
   } else {
--- src/version.c
+++ src/version.c
@@ -1,8 +1,8 @@
-/* @(#) $Header: /home/deyke/tmp/cvs/tcp/src/version.c,v 1.186 1994-04-09 15:30:36 deyke Exp $ */
+/* @(#) $Header: /home/deyke/tmp/cvs/tcp/src/version.c,v 1.187 1994-04-11 11:08:37 deyke Exp $ */
 
 #ifndef __lint
-static char id[] = "@(#)WAMPES-940409";
+static char id[] = "@(#)WAMPES-940411";
 #endif
 
 /* version control information */
-char Version[] = "WAMPES-940409";
+char Version[] = "WAMPES-940411";
