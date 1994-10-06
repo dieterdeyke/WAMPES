3647787dca9156c713664e06ee3a6ad29e7ad4e9
deyke <deyke>
1994-10-06 16:17:12 +0000

    Initial revision

---
+++ src/ipfilter.h
@@ -0,0 +1,13 @@
+/* @(#) $Header: /home/deyke/tmp/cvs/tcp/src/ipfilter.h,v 1.1 1994-10-06 16:17:12 deyke Exp $ */
+
+#ifndef _IPFILTER_H
+#define _IPFILTER_H
+
+#ifndef _GLOBAL_H
+#include "global.h"
+#endif
+
+/* In ipfilter.c: */
+int ipfilter(int32 addr);
+
+#endif  /* _IPFILTER_H */
