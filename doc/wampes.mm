1838e63f669da769519c7522d43e55c09765517c
deyke <deyke>
1995-01-26 09:19:56 +0000

    *** empty log message ***

--- doc/wampes.mm
+++ doc/wampes.mm
@@ -18,13 +18,13 @@
 .nr Hs 7 \" Empty line after all headers
 .nr Hy 1 \" Hyphenation on
 .\"
-.PF "^WAMPES Reference Manual^-\\\\nP-^Version 950116" \" Page footer
+.PF "^WAMPES Reference Manual^-\\\\nP-^Version 950126" \" Page footer
 .\"
 .S 30
 .ce
 \fBWAMPES Reference Manual\fP
 .ce
-Version 950116
+Version 950126
 .S
 .SP 2
 .S 15
--- src/version.c
+++ src/version.c
@@ -1,8 +1,8 @@
-/* @(#) $Header: /home/deyke/tmp/cvs/tcp/src/version.c,v 1.249 1995-01-16 13:20:09 deyke Exp $ */
+/* @(#) $Header: /home/deyke/tmp/cvs/tcp/src/version.c,v 1.250 1995-01-26 09:20:06 deyke Exp $ */
 
 #ifndef __lint
-static char id[] = "@(#)WAMPES-950116";
+static char id[] = "@(#)WAMPES-950126";
 #endif
 
 /* version control information */
-char Version[] = "WAMPES-950116";
+char Version[] = "WAMPES-950126";
