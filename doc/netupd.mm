e13181d6b2276b03b1ef0c4280ba344382f529a5
deyke <deyke>
1995-04-10 12:02:02 +0000

    *** empty log message ***

--- doc/netupd.mm
+++ doc/netupd.mm
@@ -18,13 +18,13 @@
 .nr Hs 7 \" Empty line after all headers
 .nr Hy 1 \" Hyphenation on
 .\"
-.PF "^Net Update Reference Manual^-\\\\nP-^Version 950405" \" Page footer
+.PF "^Net Update Reference Manual^-\\\\nP-^Version 950410" \" Page footer
 .\"
 .S 30
 .ce
 \fBNet Update Reference Manual\fP
 .ce
-Version 950405
+Version 950410
 .S
 .SP 2
 .S 15
--- src/version.c
+++ src/version.c
@@ -1,8 +1,8 @@
-/* @(#) $Header: /home/deyke/tmp/cvs/tcp/src/version.c,v 1.264 1995-04-06 06:20:43 deyke Exp $ */
+/* @(#) $Header: /home/deyke/tmp/cvs/tcp/src/version.c,v 1.265 1995-04-10 12:02:12 deyke Exp $ */
 
 #ifndef __lint
-static char id[] = "@(#)WAMPES-950405";
+static char id[] = "@(#)WAMPES-950410";
 #endif
 
 /* version control information */
-char Version[] = "WAMPES-950405";
+char Version[] = "WAMPES-950410";
