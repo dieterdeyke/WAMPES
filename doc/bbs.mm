e6dff414f007fff9c394c88a432f642dedb5806b
deyke <deyke>
1995-08-10 10:27:32 +0000

    *** empty log message ***

--- doc/bbs.mm
+++ doc/bbs.mm
@@ -12,13 +12,13 @@
 .nr Hs 7 \" Empty line after all headers
 .nr Hy 1 \" Hyphenation on
 .\"
-.PF "^BBS Reference Manual^-\\\\nP-^Version 950807" \" Page footer
+.PF "^BBS Reference Manual^-\\\\nP-^Version 950810" \" Page footer
 .\"
 .S 30
 .ce
 \fBBBS Reference Manual\fP
 .ce
-Version 950807
+Version 950810
 .S
 .SP 2
 .S 15
--- src/version.c
+++ src/version.c
@@ -1,8 +1,8 @@
-/* @(#) $Header: /home/deyke/tmp/cvs/tcp/src/version.c,v 1.286 1995-08-07 15:11:23 deyke Exp $ */
+/* @(#) $Header: /home/deyke/tmp/cvs/tcp/src/version.c,v 1.287 1995-08-10 10:28:11 deyke Exp $ */
 
 #ifndef __lint
-static char id[] = "@(#)WAMPES-950807";
+static char id[] = "@(#)WAMPES-950810";
 #endif
 
 /* version control information */
-char Version[] = "WAMPES-950807";
+char Version[] = "WAMPES-950810";
