8f25960b55b53cf608825713b4460a83ca228813
deyke <deyke>
1997-03-07 19:57:04 +0000

    None

--- doc/bridge.mm
+++ doc/bridge.mm
@@ -12,13 +12,13 @@
 .nr Hs 7 \" Empty line after all headers
 .nr Hy 1 \" Hyphenation on
 .\"
-.PF "^Bridge Reference Manual^-\\\\nP-^Version 970211" \" Page footer
+.PF "^Bridge Reference Manual^-\\\\nP-^Version 970307" \" Page footer
 .\"
 .S 30
 .ce
 \fBBridge Reference Manual\fP
 .ce
-Version 970211
+Version 970307
 .S
 .SP 2
 .S 15
--- src/version.c
+++ src/version.c
@@ -1,6 +1,6 @@
-/* @(#) $Id: version.c,v 1.339 1997-02-11 17:51:58 deyke Exp $ */
+/* @(#) $Id: version.c,v 1.340 1997-03-07 19:57:29 deyke Exp $ */
 
-static char id[] = "@(#)WAMPES-970211";
+static char id[] = "@(#)WAMPES-970307";
 
 /* version control information */
 char *Version = id + 4;
