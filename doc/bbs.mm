97bc97c244c1f46e5d509364df757bf26fb7f987
deyke <deyke>
1997-06-09 16:58:22 +0000

    None

--- doc/bbs.mm
+++ doc/bbs.mm
@@ -12,13 +12,13 @@
 .nr Hs 7 \" Empty line after all headers
 .nr Hy 1 \" Hyphenation on
 .\"
-.PF "^BBS Reference Manual^-\\\\nP-^Version 970512" \" Page footer
+.PF "^BBS Reference Manual^-\\\\nP-^Version 970609" \" Page footer
 .\"
 .S 30
 .ce
 \fBBBS Reference Manual\fP
 .ce
-Version 970512
+Version 970609
 .S
 .SP 2
 .S 15
--- src/version.c
+++ src/version.c
@@ -1,6 +1,6 @@
-/* @(#) $Id: version.c,v 1.343 1997-05-12 18:56:57 deyke Exp $ */
+/* @(#) $Id: version.c,v 1.344 1997-06-09 17:00:25 deyke Exp $ */
 
-static char id[] = "@(#)WAMPES-970512";
+static char id[] = "@(#)WAMPES-970609";
 
 /* version control information */
 char *Version = id + 4;
