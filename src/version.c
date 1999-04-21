dd6ccd9bd026991f3ff10b9f227a86fcfdeff6cc
deyke <deyke>
1999-04-21 19:56:49 +0000

    None

--- src/version.c
+++ src/version.c
@@ -1,6 +1,6 @@
-/* @(#) $Id: version.c,v 1.364 1999-02-11 19:26:49 deyke Exp $ */
+/* @(#) $Id: version.c,v 1.365 1999-04-21 19:57:10 deyke Exp $ */
 
-static char id[] = "@(#)WAMPES-990211";
+static char id[] = "@(#)WAMPES-990421";
 
 /* version control information */
 char *Version = id + 4;
