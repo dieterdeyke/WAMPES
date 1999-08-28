c8d847317ff7be8483292253bab21390f9e80c78
deyke <deyke>
1999-08-28 16:38:38 +0000

    *** empty log message ***

--- doc/netupd.mm
+++ doc/netupd.mm
@@ -12,13 +12,13 @@
 .nr Hs 7 \" Empty line after all headers
 .nr Hy 1 \" Hyphenation on
 .\"
-.PF "^Net Update Reference Manual^-\\\\nP-^Version 990710" \" Page footer
+.PF "^Net Update Reference Manual^-\\\\nP-^Version 990828" \" Page footer
 .\"
 .S 30
 .ce
 \fBNet Update Reference Manual\fP
 .ce
-Version 990710
+Version 990828
 .S
 .SP 2
 .S 15
--- src/version.c
+++ src/version.c
@@ -1,6 +1,6 @@
-/* @(#) $Id: version.c,v 1.370 1999-07-10 15:01:01 deyke Exp $ */
+/* @(#) $Id: version.c,v 1.371 1999-08-28 16:38:47 deyke Exp $ */
 
-static char id[] = "@(#)WAMPES-990710";
+static char id[] = "@(#)WAMPES-990828";
 
 /* version control information */
 char *Version = id + 4;
